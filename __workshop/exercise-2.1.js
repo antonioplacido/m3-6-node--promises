7f311476d97b5e0a6e6047d49ba3e248b10767c3
Antonio Placido <aplacido1992@gmail.com>
2020-07-10 17:04:13 -0400

    Question 2.1

--- __workshop/exercise-2.1.js
+++ __workshop/exercise-2.1.js
@@ -5,16 +5,31 @@
 const request = require("request-promise");
 
 const testGreeting = () => {
-  return request("<API_URL") // 1
+  return request("https://journeyedu.herokuapp.com/hello") // 1
     .then((response) => {
       return JSON.parse(response); // 2  // on front end with HTML + script- scripts you use response.json();
+      // console.log();
     })
     .then((parsedResponse) => {
-      return; // 3
+      // console.log();
+      return parsedResponse; // 3
     })
     .catch((err) => console.log("Error: ", err));
 };
 
 testGreeting().then((result) => console.log(result));
 
-//
+////
+
+// const request = require('request-promise');
+// const testGreeting = () => {
+//   return request('https://journeyedu.herokuapp.com/hello') // 1
+//     .then((response) => {
+//       return JSON.parse(response);
+//     })
+//     .then((parsedResponse) => {
+//       return parsedResponse.data.text;
+//     })
+//     .catch((err) => console.log('Error: ', err));
+// };
+// testGreeting().then((result) => console.log(result));
